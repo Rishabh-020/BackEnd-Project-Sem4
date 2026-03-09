95d1458a838974dde3f4d24979cfb1bd37dfa0e7
Rishabh-020 <rishabh5824sjps@gmail.com>
2026-03-09 23:34:17 +0530

    some changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express"); 
 const userRoutes = require("./routes/user.routes"); 
 const authRoutes = require("../src/routes/auth.routes"); 
+const contactRoutes= require("../src/routes/contact.routes")
 const app = express(); 
 app.use(express.json()); 
 app.use("/api/users", userRoutes); 
 app.use("/api/auth", authRoutes); 
+app.use("/api/contact", contactRoutes); 
 module.exports = app; 
  
