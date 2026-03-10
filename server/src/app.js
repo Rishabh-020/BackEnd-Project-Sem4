e8be43d2e469d4cdf9d07caeea26fbaa84773195
Rishabh-020 <rishabh5824sjps@gmail.com>
2026-03-10 16:33:45 +0530

    some changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,18 @@
 const express = require("express"); 
 const cors = require("cors");
-
-const authRoutes = require("../src/routes/auth.routes")
-const postRoutes = require("../src/routes/post.routes");
-
 const app = express(); 
+
+const postRoutes = require("../src/routes/post.routes");
+const authRoutes = require("../src/routes/auth.routes"); 
+const contactRoutes= require("../src/routes/contact.routes")
+
 app.use(express.json()); 
 
+app.use(express.json()); 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
-
-const userRoutes = require("./routes/user.routes"); 
-const authRoutes = require("../src/routes/auth.routes"); 
-const contactRoutes= require("../src/routes/contact.routes")
-const app = express(); 
-app.use(express.json()); 
-app.use("/api/users", userRoutes); 
-app.use("/api/auth", authRoutes); 
 app.use("/api/contact", contactRoutes); 
+
 module.exports = app; 
  
