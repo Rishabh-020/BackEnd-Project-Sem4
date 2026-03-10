782503e0b6fa9e4afd590f6687d9fb18b1f062ba
Kunal <sharmakunal19806@gmail.com>
2026-03-10 10:45:27 +0530

    Add Post Feature

--- server/server.js
+++ server/server.js
@@ -1,18 +1,5 @@
-const express=require('express')
-const cors =require("cors");
-const dotenv = require( "dotenv");
-const authRoutes =require( "./src/routes/auth.routes.js") // ← ADD THIS
+const app = require("./src/app");
 
-dotenv.config();
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API working" });
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
-
-app.use("/api/auth", authRoutes);  // ← ADD THIS
-
-app.listen(5000, () => console.log("Server running on 5000"));
--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,15 @@
 const express = require("express"); 
-const userRoutes = require("./routes/user.routes"); 
+const cors = require("cors");
+
+const authRoutes = require("../src/routes/auth.routes")
+const postRoutes = require("../src/routes/post.routes");
+
 const app = express(); 
 app.use(express.json()); 
-app.use("/api/users", userRoutes); 
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
 module.exports = app; 
  
