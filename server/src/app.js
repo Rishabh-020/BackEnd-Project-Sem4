6636e2ddac3044a0f4b01043738f80006276e4d4
Kunal <sharmakunal19806@gmail.com>
2026-03-12 17:22:50 +0530

    Kunal

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,21 @@
 const express = require("express"); 
-
-const userRoutes = require("./routes/user.routes"); 
-const authRoutes = require("../src/routes/auth.routes"); 
-const contactRoutes= require("../src/routes/contact.routes")
-const app = express(); 
-app.use(express.json()); 
-app.use("/api/users", userRoutes); 
-app.use("/api/auth", authRoutes); 
-app.use("/api/contact", contactRoutes); 
-
 const cors = require("cors");
 
-const authRoutes = require("../src/routes/auth.routes")
-const postRoutes = require("../src/routes/post.routes");
+const authRoutes = require("../src/routes/auth.routes"); 
+const contactRoutes = require("../src/routes/contact.routes")
+const postRoutes = require("../src/routes/post.routes")
 
-
+const app = express(); 
+app.use(cors());
 app.use(express.json()); 
 
-
+app.use("/api/contact", contactRoutes); 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
+
+
+
+
 
 module.exports = app; 
  
